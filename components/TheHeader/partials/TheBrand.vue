fda7cc11c1342b99a0c33aaf9b9fdac88fdb6011
Daniel Soares <danielsoaresf@hotmail.com>
2025-01-04 16:01:04 -0300

    feat(no-ref): add prismic in header

--- components/TheHeader/partials/TheBrand.vue
+++ components/TheHeader/partials/TheBrand.vue
@@ -7,6 +7,6 @@ defineProps<{ data: TheBrand }>();
 <template>
   <NuxtLink href="/" class="-m-1.5 p-1.5">
     <span class="sr-only">{{ data.company_name }}</span>
-    <img class="h-8 w-auto" :src="data.company_brand" :alt="data.company_name" />
+    <img class="h-8 w-auto" :src="data.company_brand.url" :alt="data.company_name" />
   </NuxtLink>
 </template>
--- components/TheHeader/types.ts
+++ components/TheHeader/types.ts
@@ -1,17 +1,17 @@
 export type TheSublink = { emoji: string; title: string; subtitle: string; path: string; target: '_self' | '_blank' };
 
-export type TheBrand = { company_name: string; company_brand: string };
+export type TheBrand = { company_name: string; company_brand: { url: string; alt: string; copyright: string } };
 
-export interface TheHeaderLink {
+export type TheHeaderLink = {
   emoji: string;
   label: string;
   path: string | null;
   target?: '_self' | '_blank';
   sublinks?: TheSublink[];
-}
+};
 
 export type TheHeader = {
   company_name: string;
-  company_brand: string;
+  company_brand: { url: string; alt: string; copyright: string };
   links: TheHeaderLink[];
 };
